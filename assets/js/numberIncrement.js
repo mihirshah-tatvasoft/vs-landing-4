36e53930015544de62afbcdc77bfc88a40636735
Akshat Mewada <akshat.mewada@tatvasoft.com>
2023-05-31 18:00:07 +0530

    Added gulp start and build tasks

--- assets/js/numberIncrement.js
+++ assets/js/numberIncrement.js
@@ -1,27 +1,116 @@
-export default class countUp {
-    constructor(el) {
-        this.el = el;
-        this.setVars();
-        this.init();
-    }
+// // class base view
+// export default class countUp {
+//     loaded = false;
+//     constructor(el) {
+//         this.el = el;
+//         this.setVars();
+//         this.init();
+//     }
 
-    setVars() {
-        this.number = this.el.querySelectorAll("[data-countup-number]");
+//     setVars() {
+//         this.number = this.el.querySelectorAll("[data-countup-number]");
+//         this.observerOptions = { root: null, rootMargin: "0px 0px", threshold: 0 };
+//         this.observer = new IntersectionObserver((entries) => {
+//             entries.forEach((entry) => {
+//                 const end = parseFloat(
+//                     entry.target.dataset.countupNumber.replace(/,/g, "")
+//                 );
+//                 const decimals = this.countDecimals(end);
+//                 if (entry.isIntersecting) {
+//                     this.iterateValue(entry.target, end, decimals);
+//                     this.unobserve(entry.target);
+//                 }
+//             });
+//         }, this.observerOptions);
+//     }
+
+//     unobserve(element) {
+//         this.observer.unobserve(element);
+//     }
+
+//     init() {
+//         if (this.number.length > 0) {
+//             this.number.forEach((el) => {
+//                 this.observer.observe(el);
+//             });
+//         }
+//     }
+
+//     iterateValue(el, end, decimals) {
+//         const start = 0;
+//         const duration = 2500;
+//         let startTimestamp = null;
+
+//         const step = (timestamp) => {
+//             if (!startTimestamp) startTimestamp = timestamp;
+//             const elapsedPercent = (timestamp - startTimestamp) / duration;
+//             const easedProgress = Math.min(this.easeOutQuint(elapsedPercent), 1);
+//             let interimNumber = Math.abs(easedProgress * (end - start) + start);
+//             el.innerHTML = this.formatNumber(interimNumber, decimals);
+//             if (easedProgress < 1) {
+//                 window.requestAnimationFrame(step);
+//             }
+//         };
+
+//         // requestAnimationFrame returns DOMHighResTimeStamp as a callback (used as timestamp)
+//         window.requestAnimationFrame(step);
+//     }
+
+//     easeOutQuad(x) {
+//         return 1 - Math.pow(1 - x, 3);
+//     }
+
+//     easeOutQuint(x) {
+//         return 1 - Math.pow(1 - x, 5);
+//     }
+
+//     countDecimals(val) {
+//         if (Math.floor(val) === val) return 0;
+//         return val.toString().split(".")[1].length || 0;
+//     }
+
+//     formatNumber(val, decimals) {
+//         return val.toLocaleString("en-US", {
+//             minimumFractionDigits: decimals,
+//             maximumFractionDigits: decimals
+//         });
+//     }
+// }
+
+// // You CAN use this pattern, but it's single purpose right now
+// const dataModules = [...document.querySelectorAll('[data-module="countup"]')];
+
+// dataModules.forEach((element) => {
+//     element.dataset.module.split(" ").forEach(function () {
+//         new countUp(element);
+//     });
+// });
+
+
+// // Function base view
+function countUp(el) {
+    let loaded = false;
+
+    function setVars() {
+        this.number = el.querySelectorAll("[data-countup-number]");
         this.observerOptions = { root: null, rootMargin: "0px 0px", threshold: 0 };
         this.observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
-                const end = parseFloat(
-                    entry.target.dataset.countupNumber.replace(/,/g, "")
-                );
-                const decimals = this.countDecimals(end);
+                const end = parseFloat(entry.target.dataset.countupNumber.replace(/,/g, ""));
+                const decimals = countDecimals(end);
                 if (entry.isIntersecting) {
-                    this.iterateValue(entry.target, end, decimals);
+                    iterateValue(entry.target, end, decimals);
+                    unobserve(entry.target);
                 }
             });
         }, this.observerOptions);
     }
 
-    init() {
+    function unobserve(element) {
+        this.observer.unobserve(element);
+    }
+
+    function init() {
         if (this.number.length > 0) {
             this.number.forEach((el) => {
                 this.observer.observe(el);
@@ -29,52 +118,53 @@ export default class countUp {
         }
     }
 
-    iterateValue(el, end, decimals) {
+    function iterateValue(el, end, decimals) {
         const start = 0;
         const duration = 2500;
         let startTimestamp = null;
 
-        const step = (timestamp) => {
+        function step(timestamp) {
             if (!startTimestamp) startTimestamp = timestamp;
             const elapsedPercent = (timestamp - startTimestamp) / duration;
-            const easedProgress = Math.min(this.easeOutQuint(elapsedPercent), 1);
+            const easedProgress = Math.min(easeOutQuint(elapsedPercent), 1);
             let interimNumber = Math.abs(easedProgress * (end - start) + start);
-            el.innerHTML = this.formatNumber(interimNumber, decimals);
+            el.innerHTML = formatNumber(interimNumber, decimals);
             if (easedProgress < 1) {
                 window.requestAnimationFrame(step);
             }
-        };
+        }
 
-        // requestAnimationFrame returns DOMHighResTimeStamp as a callback (used as timestamp)
         window.requestAnimationFrame(step);
     }
 
-    easeOutQuad(x) {
+    function easeOutQuad(x) {
         return 1 - Math.pow(1 - x, 3);
     }
 
-    easeOutQuint(x) {
+    function easeOutQuint(x) {
         return 1 - Math.pow(1 - x, 5);
     }
 
-    countDecimals(val) {
+    function countDecimals(val) {
         if (Math.floor(val) === val) return 0;
         return val.toString().split(".")[1].length || 0;
     }
 
-    formatNumber(val, decimals) {
+    function formatNumber(val, decimals) {
         return val.toLocaleString("en-US", {
             minimumFractionDigits: decimals,
             maximumFractionDigits: decimals
         });
     }
+
+    setVars();
+    init();
 }
 
-// You CAN use this pattern, but it's single purpose right now
 const dataModules = [...document.querySelectorAll('[data-module="countup"]')];
 
 dataModules.forEach((element) => {
     element.dataset.module.split(" ").forEach(function () {
-        new countUp(element);
+        countUp(element);
     });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,66 @@
-/*
- * Simple GULP tooling
- */
-const gulp = require("gulp");
-const sass = require('gulp-sass');
-const prefix = require("gulp-autoprefixer");
-const minify = require("gulp-clean-css");
-const rename = require("gulp-rename");
+const { src, dest, series, watch } = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const gulpif = require('gulp-if');
+const cleanCss = require('gulp-clean-css');
+let buildPath = '';
 
-gulp.task("watch", function () {
-  gulp
-    .src("./assets/css/scss/*.scss")
-    .pipe(sass())
-    .pipe(prefix())
-    .pipe(minify())
-    .pipe(
-      rename(function (path) {
-        return {
-          dirname: path.dirname + "",
-          basename: path.basename + ".min",
-          extname: ".css",
-        };
-      })
-    )
-    .pipe(gulp.dest("./assets/css"));
-});
+const jsBuild = () => {
+    return src([
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        'assets/js/tilt.js',
+        './node_modules/typewriter-effect/dist/core.js',
+        './node_modules/gsap/dist/gsap.min.js',
+        './node_modules/gsap/dist/ScrollTrigger.min.js',
+        'assets/js/ScrollSmoother.js',
+        'assets/js/numberIncrement.js',
+        'assets/js/splitType.js',
+        'assets/js/custom.js',
+        'assets/js/landingAnimation.js'
+    ])
+        .pipe(gulpif(buildPath == '', sourcemaps.init()))
+        .pipe(concat('main.js'))
+        .pipe(gulpif(buildPath == '', sourcemaps.write()))
+        .pipe(gulpif(buildPath != '', uglify()))
+        .pipe(dest(buildPath + 'assets/js/'));
+}
 
-gulp.task("open", function () {
-  // gulp.src("./index.html").pipe(open());
-});
+const cssBuild = () => {
+    return src('assets/css/style.scss')
+        .pipe(gulpif(buildPath == '', sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulpif(buildPath == '', sourcemaps.write()))
+        .pipe(gulpif(buildPath != '', cleanCss()))
+        .pipe(rename('main.css'))
+        .pipe(dest(buildPath + 'assets/css/'));
+}
 
-gulp.task("default", gulp.parallel("open", "watch"));
+function start(cb) {
+    series(jsBuild, cssBuild)();
+    watch(['assets/js/*.js', '!assets/js/main.js'], (cb) => {
+        jsBuild();
+        cb();
+    });
+    watch('assets/css/*.scss', (cb) => {
+        cssBuild();
+        cb();
+    });
+    cb();
+}
+
+function build(cb) {
+    buildPath = 'build/';
+    series(jsBuild, cssBuild)();
+    src(['assets/**/*', '!assets/css/*', '!assets/js/*'])
+        .pipe(dest(buildPath + 'assets'));
+    src('*.html')
+        .pipe(dest(buildPath));
+    cb();
+}
+
+exports.start = start;
+exports.default = start;
+exports.build = build;
